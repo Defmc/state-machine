8e552583515f10d8ea78f9dced0d9f151a0661b6
Defmc <defmc0101@gmail.com>
2021-05-22 14:29:03 -0300

    Removed unecessary comment

--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -1,22 +1,3 @@
-#01. tool
-#02. class_name
-#03. extends
-#04. # docstring
-#
-#05. signals
-#06. enums
-#07. constants
-#08. exported variables
-#09. public variables
-#10. private variables
-#11. onready variables
-#
-#12. optional built-in virtual _init method
-#13. built-in virtual _ready method
-#14. remaining built-in virtual methods
-#15. public methods
-#16. private methods
-
 class_name Player
 
 extends KinematicBody2D
